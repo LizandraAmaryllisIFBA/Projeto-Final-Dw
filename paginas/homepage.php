cf9e683b6a1bcf5ae679a74a1b768409979174d0
wxxbel <isabelsofiasilva0@gmail.com>
2025-11-27 17:08:09 -0300

    Criação da homepage;
    Foi criado o arquivo da homepage e os div das barras laterais e da barra main

---
+++ paginas/homepage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+
+        <title>Homepage</title>
+    </head>
+
+    <body>
+
+        <!-- layout da pagina -->
+        <div class="layout">
+
+            <!-- layout da barra lateral esquerda -->
+            <div class="layout__left-sidebar">
+
+            </div>
+
+            <!-- layout do main -->
+            <div class="layout_main">
+
+            </div>
+
+            <!-- layout da barra lateral direita -->
+            <div class="layout__right-side-bar-conteiner">
+
+            </div>
+
+        </div>
+    </body>
+</html>
